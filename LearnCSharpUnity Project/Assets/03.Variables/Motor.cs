fef70a54df0f364f31f1fb0b9a750988dbeb5ef6
Moon Antonio <pinchodev91@gmail.com>
2018-08-19 08:16:37 +0200

    Creado mini sistema solar

---
+++ LearnCSharpUnity Project/Assets/03.Variables/Motor.cs
@@ -0,0 +1,28 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Motor.cs (00/00/0000)														\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using System.Collections;
+#endregion
+
+namespace MoonAntonio
+{
+	public class Motor : MonoBehaviour 
+	{
+		public float vel;
+
+		void Update()
+		{
+			float velFrameLimitada = vel * Time.deltaTime;
+			Vector3 rotacion = new Vector3(0, velFrameLimitada, 0);
+			transform.Rotate(rotacion);
+		}
+	}
+}
