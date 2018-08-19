e039f0bab162079fdadd83c45e74d30f6384fafd
Moon Antonio <pinchodev91@gmail.com>
2018-08-19 07:26:04 +0200

    Creado diferentes nombres para los humanos

---
+++ LearnCSharpUnity Project/Assets/03.Variables/Humano.cs
@@ -0,0 +1,23 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Humano.cs (00/00/0000)														\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio
+{
+	public class Humano : MonoBehaviour 
+	{
+		private void Start()
+		{
+			Debug.Log("Hola, mi nombre es " + name);
+		}
+	}
+}
