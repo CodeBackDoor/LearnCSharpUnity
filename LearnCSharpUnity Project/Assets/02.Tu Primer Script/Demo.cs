259b98890b108ca2de207a26be9319a9086fb037
Moon Antonio <pinchodev91@gmail.com>
2018-08-19 06:43:56 +0200

    Primer script creado

---
+++ LearnCSharpUnity Project/Assets/02.Tu Primer Script/Demo.cs
@@ -0,0 +1,28 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Demo.cs (00/00/0000)															\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio
+{
+	public class Demo : MonoBehaviour 
+	{
+		private void Start()
+		{
+			Debug.Log("Tiempo: " + Time.timeSinceLevelLoad);
+		}
+
+		private void Update()
+		{
+			Debug.Log("Tiempo: " + Time.timeSinceLevelLoad);
+		}
+	}
+}
