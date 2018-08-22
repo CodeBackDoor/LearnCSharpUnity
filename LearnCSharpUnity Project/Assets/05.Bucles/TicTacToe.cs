b1b5d7272a88346ee8d640f3dbb93b2a711f13ec
Moon Antonio <pinchodev91@gmail.com>
2018-08-23 01:31:26 +0200

    celdas sin bucle

---
+++ LearnCSharpUnity Project/Assets/05.Bucles/TicTacToe.cs
@@ -0,0 +1,41 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// TicTacToe.cs (00/00/0000)													\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+#endregion
+
+namespace MoonAntonio
+{
+	public class TicTacToe : MonoBehaviour 
+	{
+		[SerializeField] Text[] cells;
+
+		void Start()
+		{
+			NewGame();
+		}
+
+		public void NewGame()
+		{
+			cells[0].text = "";
+			cells[1].text = "";
+			cells[2].text = "";
+			cells[3].text = "";
+			cells[4].text = "";
+			cells[5].text = "";
+			cells[6].text = "";
+			cells[7].text = "";
+			cells[8].text = "";
+			cells[9].text = "";
+		}
+	}
+}
