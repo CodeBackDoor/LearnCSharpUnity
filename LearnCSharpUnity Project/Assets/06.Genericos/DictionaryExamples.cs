50fa10964f9f9eb49e1bd529d56802a9a65ed9df
Moon Antonio <pinchodev91@gmail.com>
2018-08-23 03:11:05 +0200

    Introduccion a los diccionarios

---
+++ LearnCSharpUnity Project/Assets/06.Genericos/DictionaryExamples.cs
@@ -0,0 +1,30 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// DictionaryExamples.cs (00/00/0000)											\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+#endregion
+
+namespace MoonAntonio
+{
+	public class DictionaryExamples : MonoBehaviour 
+	{
+		public Dictionary<string, int> stats = new Dictionary<string, int>();
+
+		void Start()
+		{
+			stats.Add("HP", 10);
+			stats.Add("MP", 3);
+			stats.Remove("MP");
+			if (stats.ContainsKey("HP")) Debug.Log(string.Format("Tienes {0} puntos de golpe restantes", stats["HP"]));
+		}
+	}
+}
