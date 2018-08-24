b8e8ed5dc7b2b0740052b6dd8c11a2d3cc128765
Moon Antonio <pinchodev91@gmail.com>
2018-08-24 03:37:11 +0200

    Corrutinas nativas

---
+++ LearnCSharpUnity Project/Assets/07.Corrutinas/CorrutinaNativa.cs
@@ -0,0 +1,44 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// CorrutinaNativa.cs (00/00/0000)												\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using System.Collections;
+#endregion
+
+namespace MoonAntonio
+{
+	public class CorrutinaNativa : MonoBehaviour 
+	{
+		IEnumerator trend;
+
+		void Start()
+		{
+			trend = TrendLine();
+		}
+
+		void Update()
+		{
+			if (trend.MoveNext())
+				Debug.Log((int)trend.Current);
+		}
+
+		IEnumerator TrendLine()
+		{
+			int value = 0;
+			while (true)
+			{
+				value += UnityEngine.Random.Range(-1, 2);
+				yield return value;
+				if (Mathf.Abs(value) >= 10)
+					yield break;
+			}
+		}
+	}
+}
