7e8aef1bad7b33006c9e47f7c540a4f75cf6c1d7
Moon Antonio <pinchodev91@gmail.com>
2018-08-24 13:06:03 +0200

    Preparar una herencia

---
+++ LearnCSharpUnity Project/Assets/08.Clases/Polimorfismo.cs
@@ -0,0 +1,30 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Polimorfismo.cs (00/00/0000)													\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+#endregion
+
+namespace MoonAntonio
+{
+	public class Polimorfismo : MonoBehaviour 
+	{
+		void Start()
+		{
+			List<System.Object> myList = new List<System.Object>();
+			myList.Add("Hola Mundo!");
+			myList.Add(42);
+			myList.Add(Camera.main);
+			for (int i = 0; i < myList.Count; ++i)
+				Debug.Log(i + ": " + myList[i].ToString());
+		}
+	}
+}
---
+++ LearnCSharpUnity Project/Assets/08.Clases/Selectable.cs
@@ -0,0 +1,33 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Selectable.cs (00/00/0000)													\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using System.Collections;
+#endregion
+
+namespace MoonAntonio
+{
+	public abstract class Selectable : MonoBehaviour
+	{
+		public bool isSelected { get; protected set; }
+		public virtual void SetSelected(bool value)
+		{
+			if (isSelected == value)
+				return;
+			isSelected = value;
+			if (isSelected)
+				Select();
+			else
+				Deselect();
+		}
+		protected abstract void Select();
+		protected abstract void Deselect();
+	}
+}
