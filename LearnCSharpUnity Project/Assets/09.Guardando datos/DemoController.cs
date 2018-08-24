4c392e0b88d2e53e452fcfad53a6abe8aae3ff4b
Moon Antonio <pinchodev91@gmail.com>
2018-08-24 18:51:49 +0200

    Guardando con JSON

---
+++ LearnCSharpUnity Project/Assets/09.Guardando datos/DemoController.cs
@@ -0,0 +1,65 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// DemoController.cs (00/00/0000)												\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using MiniJSON;
+#endregion
+
+namespace MoonAntonio
+{
+	public class DemoController : MonoBehaviour 
+	{
+		public Monster monster;
+
+		void Start()
+		{
+			Load();
+		}
+
+		void OnGUI()
+		{
+			if (GUI.Button(new Rect(10, 10, 100, 30), "Random"))
+				Randomize();
+			if (GUI.Button(new Rect(10, 50, 100, 30), "Guardar"))
+				Save();
+
+			if (GUI.Button(new Rect(10, 90, 100, 30), "Cargar"))
+				Load();
+		}
+
+		void Randomize()
+		{
+			monster.HP = UnityEngine.Random.Range(10, 100);
+			float x = UnityEngine.Random.Range(-10f, 10f);
+			float y = UnityEngine.Random.Range(-10f, 10f);
+			float z = UnityEngine.Random.Range(-10f, 10f);
+			monster.transform.localPosition = new Vector3(x, y, z);
+		}
+
+		void Save()
+		{
+			string json = Json.Serialize(monster.Save());
+			Debug.Log(json);
+			PlayerPrefs.SetString("Monster", json);
+		}
+
+		void Load()
+		{
+			string json = PlayerPrefs.GetString("Monster", string.Empty);
+			if (!string.IsNullOrEmpty(json))
+			{
+				var dict = Json.Deserialize(json) as Dictionary<string, object>;
+				monster.Load(dict);
+			}
+		}
+	}
+}
--- LearnCSharpUnity Project/Assets/09.Guardando datos/Monster.cs
+++ LearnCSharpUnity Project/Assets/09.Guardando datos/Monster.cs
@@ -1,7 +1,7 @@
 ﻿//                                  ┌∩┐(◣_◢)┌∩┐
 //																				\\
-// Monster.cs (00/00/0000)													\\
-// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com								        \\
+// Monster.cs (00/00/0000)														\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
 // Descripcion:																	\\
 // Fecha Mod:		00/00/0000													\\
 // Ultima Mod:																	\\
@@ -9,12 +9,35 @@
 
 #region Librerias
 using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 #endregion
 
 namespace MoonAntonio
 {
 	public class Monster : MonoBehaviour 
 	{
+		public int HP;
 
+		public virtual Dictionary<string, object> Save()
+		{
+			Dictionary<string, object> data = new Dictionary<string, object>();
+			data.Add("HP", HP);
+			data.Add("X", transform.localPosition.x);
+			data.Add("Y", transform.localPosition.y);
+			data.Add("Z", transform.localPosition.z);
+			data.Add("Prefab", gameObject.name);
+			return data;
+		}
+
+		public virtual void Load(Dictionary<string, object> data)
+		{
+			HP = Convert.ToInt32(data["HP"]);
+			float x = Convert.ToSingle(data["X"]);
+			float y = Convert.ToSingle(data["Y"]);
+			float z = Convert.ToSingle(data["Z"]);
+			transform.localPosition = new Vector3(x, y, z);
+		}
 	}
 }
