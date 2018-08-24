e31723129cbda9f3db423108d841cbab2ca74f8f
Moon Antonio <pinchodev91@gmail.com>
2018-08-24 03:17:04 +0200

    Agregado corrutinas de unity

---
+++ LearnCSharpUnity Project/Assets/07.Corrutinas/UnityCorrutinas.cs
@@ -0,0 +1,40 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// UnityCorrutinas.cs (00/00/0000)												\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using System.Collections;
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio
+{
+	public class UnityCorrutinas : MonoBehaviour 
+	{
+		void OnEnable()
+		{
+			StartCoroutine("DoStuff");
+		}
+
+		void OnDisable()
+		{
+			StopCoroutine("DoStuff");
+		}
+
+		IEnumerator DoStuff()
+		{
+			int value = 0;
+			while (true)
+			{
+				yield return new WaitForSeconds(1);
+				value++;
+				Debug.Log("Valor:" + value);
+			}
+		}
+	}
+}
