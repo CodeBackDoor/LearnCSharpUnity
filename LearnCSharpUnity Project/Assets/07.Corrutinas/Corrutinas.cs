1a4604cb69fa2fb14dea9a48340087dc3b5ad936
Moon Antonio <pinchodev91@gmail.com>
2018-08-24 03:22:23 +0200

    Mas corrutinas

--- LearnCSharpUnity Project/Assets/07.Corrutinas/Corrutinas.cs
+++ LearnCSharpUnity Project/Assets/07.Corrutinas/Corrutinas.cs
@@ -1,7 +1,7 @@
 ﻿//                                  ┌∩┐(◣_◢)┌∩┐
 //																				\\
 // Corrutinas.cs (00/00/0000)													\\
-// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com								        \\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
 // Descripcion:																	\\
 // Fecha Mod:		00/00/0000													\\
 // Ultima Mod:																	\\
@@ -9,12 +9,32 @@
 
 #region Librerias
 using UnityEngine;
+using System.Collections;
 #endregion
 
 namespace MoonAntonio
 {
 	public class Corrutinas : MonoBehaviour 
 	{
+		public Vector3[] waypoints;
+		public float speed;
 
+		void OnEnable()
+		{
+			StartCoroutine(DoStuff());
+		}
+
+		IEnumerator DoStuff()
+		{
+			for (int i = 0; i < waypoints.Length; ++i)
+			{
+				while (transform.position != waypoints[i])
+				{
+					yield return null;
+					transform.position = Vector3.MoveTowards(transform.position, waypoints[i], speed * Time.deltaTime);
+				}
+			}
+			Debug.Log("Completado!");
+		}
 	}
 }
